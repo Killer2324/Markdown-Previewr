086b12c85572b306532d12796322f33c97fd6ef3
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-04-13 18:02:54 -0500

    config img added

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,9 +1,11 @@
+import configuration from '../../assets/icons/configuration.png'
 import '../../styles/Menu.css'
 
 const Menu: React.FC = (): JSX.Element => {
   return (
     <header className="menu--container">
       <h1 className="menu--title">Markdown Previewr</h1>
+      <img id="config--img" src={configuration} alt="config img" />
     </header>
   )
 }
