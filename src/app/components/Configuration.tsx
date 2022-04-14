2e89108cdf2cfe4fcc137b497cfbff8ca8742b69
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-04-14 16:46:33 -0500

    add rest of options into Config component

--- src/app/components/Configuration.tsx
+++ src/app/components/Configuration.tsx
@@ -27,11 +27,41 @@ const Configuration: React.FC<Props> = ({ setIsOpen }): JSX.Element => {
         <div className="config--option-container">
           <span className="option--title">Theme</span>
           <div className="config--option-item">
-            <select name="options" id="options" onChange={handleChooseTheme}>
+            <select className="options" onChange={handleChooseTheme}>
               <option selected>Choose Theme</option>
               <option value="vs-dark">vs-dark</option>
               <option value="vs">vs</option>
               <option value="hc-black">hc-black</option>
+            </select>
+          </div>
+        </div>
+        <div className="config--option-container">
+          <div className="option--title">Wordwrap</div>
+          <div className="config--option-item">
+            <select className="options">
+              <option selected>Choose Wordwrap Type</option>
+              <option value="on">on</option>
+              <option value="off">off</option>
+              <option value="wordWrapColumn">wordWrapColumn</option>
+              <option value="bounded">bounded</option>
+            </select>
+          </div>
+        </div>
+        <div className="config--option-container">
+          <div className="option--title">Minimap</div>
+          <div className="config--option-item">
+            <select className="options">
+              <option selected>Enable or Disable</option>
+              <option value="on">enable</option>
+              <option value="off">disable</option>
+            </select>
+          </div>
+        </div>
+        <div className="config--option-container">
+          <div className="option--title">Font size</div>
+          <div className="config--option-item">
+            <select className="options">
+              <option selected>Choose Font Size</option>
             </select>
           </div>
         </div>
