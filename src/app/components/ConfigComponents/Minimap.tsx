1279e03cbe26be148bf1600e5144e765d37211a0
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-04-14 17:22:41 -0500

    function to change minimap enable added

--- src/app/components/ConfigComponents/Minimap.tsx
+++ src/app/components/ConfigComponents/Minimap.tsx
@@ -1,12 +1,40 @@
+import { useEditorContext } from '../../context/EditorContext'
+
 const Minimap: React.FC = (): JSX.Element => {
+  const { options, setOptions } = useEditorContext()
+
+  const handleChooseMinimapIsEnable = (e: any): void => {
+    if (e.target.value === 'enable') {
+      const isEnable: boolean = true
+      setOptions({
+        ...options,
+        optionsConfig: {
+          ...options.optionsConfig,
+          minimap: { enabled: isEnable },
+        },
+      })
+    }
+
+    if (e.target.value === 'disable') {
+      const isEnable: boolean = false
+      setOptions({
+        ...options,
+        optionsConfig: {
+          ...options.optionsConfig,
+          minimap: { enabled: isEnable },
+        },
+      })
+    }
+  }
+
   return (
     <div className="config--option-container">
       <div className="option--title">Minimap</div>
       <div className="config--option-item">
-        <select className="options">
+        <select className="options" onChange={handleChooseMinimapIsEnable}>
           <option selected>Enable or Disable</option>
-          <option value="on">enable</option>
-          <option value="off">disable</option>
+          <option value="enable">enable</option>
+          <option value="disable">disable</option>
         </select>
       </div>
     </div>
